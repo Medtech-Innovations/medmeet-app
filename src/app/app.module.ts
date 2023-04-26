7144b3c25322f4acfeb085ec4c20059d5cdf9f5e
Liliana Fu <lilifuye72@gmail.com>
2023-04-26 18:13:52 -0500

    feat: add login button

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
 
 // Components
 import { LoginComponent } from './login/login.component';
@@ -26,6 +27,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    FormsModule,
     ReactiveFormsModule,
     MatIconModule,
     MatInputModule,
