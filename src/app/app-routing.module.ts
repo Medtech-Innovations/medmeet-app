bdfcb128a363b6cc72aff97f226cb0eb9c12bb7f
Liliana Fu <lilifuye72@gmail.com>
2023-04-28 00:11:11 -0500

    feat: add features domain

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './authentication/pages/login/login.component';
+import { RegisterComponent } from './authentication/pages/register/register.component';
 
 const routes: Routes = [
-  { path: '', component: LoginComponent },
+  { path: 'login', component: LoginComponent },
   { path: '**', component: Error },
   { path: 'register', component: RegisterComponent },
 ];
