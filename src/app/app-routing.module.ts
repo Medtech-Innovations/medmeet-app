b1a8bd5f34170f67a262c0f653169a7e337a0f8a
RamiroVT <u202123714@upc.edu.pe>
2023-05-04 16:26:54 -0500

    fix: issues with routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,14 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { LoginComponent } from './authentication/pages/login/login.component';
 import { RegisterComponent } from './authentication/pages/register/register.component';
+import { BookAppointmentComponent } from './patient/pages/book-appointment/book-appointment.component';
 import { CountrySelectorComponent } from './shared/pages/country-selector/country-selector.component';
 import { ForgotPasswordComponent } from "./authentication/pages/forgot-password/forgot-password.component";
 
-// { path: '**', component: Error },
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  { path: 'forgot-password', component: ForgotPasswordComponent }
+  { path: 'forgot-password', component: ForgotPasswordComponent },
+  { path: 'book', component: BookAppointmentComponent }
 ];
 
 @NgModule({
