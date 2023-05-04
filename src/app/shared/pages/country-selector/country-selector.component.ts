fe143d1f4bd4b078476816a4eba60dd77458f0bb
Liliana Fu <lilifuye72@gmail.com>
2023-05-04 00:39:46 -0500

    feat: add country selector api service for register

---
+++ src/app/shared/pages/country-selector/country-selector.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { CountryNameService } from '../../services/countries/countries.service';
+
+@Component({
+  selector: 'app-country-selector',
+  templateUrl: './country-selector.component.html',
+  styleUrls: ['./country-selector.component.css']
+})
+export class CountrySelectorComponent {
+  countries: string[] = [];
+  selectedCountry?: string;
+
+  constructor(private countryService: CountryNameService) { }
+
+  ngOnInit() {
+    this.countryService.getCountries().subscribe(countries => {
+      this.countries = countries.sort();
+    });
+  }
+  onCountrySelected(country: string) {
+    this.selectedCountry = country;
+  }
+}
